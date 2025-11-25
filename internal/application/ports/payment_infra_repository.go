36985239e573b41c05b5a5e3972d97e7d00bdf46
Carlos Andrés Salazar Melita <andres.melita.dev@gmail.com>
2025-11-25 19:47:35 -0300

    feat: repository interface 🎓

--- internal/application/ports/payment_infra_repository.go
+++ internal/application/ports/payment_infra_repository.go
@@ -8,4 +8,7 @@ import (
 // PaymentInfraRepository defines the repository interface for payment infrastructure data
 type PaymentInfraRepository interface {
 	GetPaymentInfraByID(ctx context.Context, paymentRackID string) (*model.PaymentInfra, error)
+	GetAvailableLockers(ctx context.Context, paymentRackID int, bookingTimeID int) (*model.AvailableLockers, error)
+	ValidateDiscountCoupon(ctx context.Context, couponCode string) (*model.DiscountCouponValidation, error)
+	GeneratePurchaseOrder(ctx context.Context, groupID int, couponCode *string, userEmail string, userPhone string) (*model.PurchaseOrder, error)
 }
