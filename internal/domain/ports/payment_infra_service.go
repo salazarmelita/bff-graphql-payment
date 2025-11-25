07086af6b1a85de265276485ebdf78ad6776cc93
Carlos Andrés Salazar Melita <andres.melita.dev@gmail.com>
2025-11-25 20:05:15 -0300

    feat: defined payment infrastructure service interface 🧩

--- internal/domain/ports/payment_infra_service.go
+++ internal/domain/ports/payment_infra_service.go
@@ -5,7 +5,10 @@ import (
 	"graphql-payment-bff/internal/domain/model"
 )
 
-// PaymentInfraService defines the use case for payment infrastructure operations
+// PaymentInfraService define el caso de uso para operaciones de infraestructura de pagos
 type PaymentInfraService interface {
 	GetPaymentInfraByID(ctx context.Context, paymentRackID string) (*model.PaymentInfra, error)
+	GetAvailableLockers(ctx context.Context, paymentRackID int, bookingTimeID int) (*model.AvailableLockers, error)
+	ValidateDiscountCoupon(ctx context.Context, couponCode string) (*model.DiscountCouponValidation, error)
+	GeneratePurchaseOrder(ctx context.Context, groupID int, couponCode *string, userEmail string, userPhone string) (*model.PurchaseOrder, error)
 }
