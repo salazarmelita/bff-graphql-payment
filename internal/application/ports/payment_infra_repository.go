7533ad42893d983b0d19d8d9fe20f8c190bafce8
Carlos Andrés Salazar Melita <andres.melita.dev@gmail.com>
2025-11-30 00:00:53 -0300

    fix: updated interface - GetPaymentInfraByID 📚

--- internal/application/ports/payment_infra_repository.go
+++ internal/application/ports/payment_infra_repository.go
@@ -7,7 +7,7 @@ import (
 
 // PaymentInfraRepository define la interfaz del repositorio para datos de infraestructura de pagos
 type PaymentInfraRepository interface {
-	GetPaymentInfraByID(ctx context.Context, paymentRackID string) (*model.PaymentInfra, error)
+	GetPaymentInfraByQrValue(ctx context.Context, qrValue string) (*model.PaymentInfra, error)
 	GetAvailableLockers(ctx context.Context, paymentRackID int, bookingTimeID int) (*model.AvailableLockers, error)
 	ValidateDiscountCoupon(ctx context.Context, couponCode string) (*model.DiscountCouponValidation, error)
 	GeneratePurchaseOrder(ctx context.Context, groupID int, couponCode *string, userEmail string, userPhone string) (*model.PurchaseOrder, error)
